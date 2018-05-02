4f73dff481cc3d16155dc8a0a3b887052c0f58cf
Anders Ekelund <anek25@ecster.se>
2018-05-02 07:36:29 +0200

    Initial commit

---
+++ IdSrv4Demo_MvcClient/IdSrv4Demo_MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using IdSrv4Demo_MvcClient.Models;
+using Newtonsoft.Json.Linq;
+
+namespace IdSrv4Demo_MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var list = GetList();
+            return View();
+        }
+
+        private async Task<string> GetList()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return string.Empty;
+            }
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return string.Empty;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/api/values");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+                return content;
+            }
+
+            return string.Empty;
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IdSrv4Demo_MvcClient/IdSrv4Demo_MvcClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdSrv4Demo_MvcClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
